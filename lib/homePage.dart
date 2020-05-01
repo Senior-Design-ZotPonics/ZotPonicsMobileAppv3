a5a91d802f2a10450f11acf2eab7e1d746d71ed8
Sidney Lau <sidnel1@uci.edu>
2020-04-30 17:06:29 -0700

    Changed app icon

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/demoPage.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
