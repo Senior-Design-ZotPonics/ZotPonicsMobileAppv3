b76465a58d180f52a41254fc40b5732c7c6c4cde
meghakak <meghakak1@gmail.com>
2022-01-19 23:00:35 -0800

    Finish general design.

--- lib/socialMediaWidget.dart
+++ lib/socialMediaWidget.dart
@@ -44,14 +44,14 @@ class _SocialMediaWidget extends State<SocialMediaWidget> {
         body: Column(
           children: [
             Text(
-              "\nZotPonics",
+              "\n\nZotPonics",
               textAlign: TextAlign.center,
               style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green.shade900, fontSize: 40)
             ),
             Text(
-                "A Sustainable Hydroponic System",
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green.shade900, fontSize: 20)
+              "A Sustainable Hydroponic System",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green.shade900, fontSize: 20),
             ),
             Row(
               children: [
@@ -63,34 +63,54 @@ class _SocialMediaWidget extends State<SocialMediaWidget> {
                       border: Border.all(color: Colors.white, width: 3)
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 30.0, top: 70.0, bottom: 70.0, right: 30.0),
-                    child: Text("My Top Plant \n" + "_topPlant", textAlign: TextAlign.center,),
+                    padding: const EdgeInsets.only(left: 35.0, top: 70.0, bottom: 70.0, right: 35.0),
+                    child: Text(
+                      "My Top Plant\n" + "_topPlant",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.bold)
+                    ),
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(top: 80.0),
+                  margin: EdgeInsets.only(top: 80.0, left: 20.0),
                   decoration: BoxDecoration(
-                      color: Colors.green,
+                      color: Colors.green.shade400,
                       shape: BoxShape.circle,
                       border: Border.all(color: Colors.white, width: 3)
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.all(50.0),
-                    child: Text("Avg Humidity \n" + "###", textAlign: TextAlign.center,),
+                    padding: const EdgeInsets.only(top: 60.0, bottom: 60.0, left: 30.0, right: 30.0),
+                    child: Text("Avg Humidity\n" + "###", textAlign: TextAlign.center,),
                   ),
                 ),
               ],
             ),
-            Container(
-              margin: EdgeInsets.only(top: 10.0),
-              decoration: BoxDecoration(
-                  color: Colors.green,
-                  shape: BoxShape.circle
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(100.0),
-                child: Text("My Top Plant \n" + "_topPlant", textAlign: TextAlign.center,),
-              ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green.shade300,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 3)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15.0, top: 55.0, bottom: 55.0, right: 15.0),
+                    child: Text("Avg Temperature\n" + "###", textAlign: TextAlign.center,),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30.0),
+                  decoration: BoxDecoration(
+                      color: Colors.green.shade600,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 3)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 80.0, bottom: 80.0),
+                    child: Text("Number of Plants Grown\n" + "###", textAlign: TextAlign.center,),
+                  ),
+                ),
+              ]
             ),
           ],
         )
